3060db4a15c31d250bca4bcf6f93e1353f2b842e
Gabriel Marques <gabrielbr201657@gmail.com>
2025-11-07 10:16:10 -0300

    feat: add CORS no FastAPI

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,19 @@
 from typing import Union
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from app.create_base import router as base_router
 from app.Rag_router import router as Rag_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
